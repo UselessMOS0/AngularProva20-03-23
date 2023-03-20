47a4bdb75d74f2cff510d9ab00e93b6588d21f07
UselessMOS0 <bono.walter@iisgalvanimi.edu.it>
2023-03-20 09:32:08 +0000

    5

--- cocktails/src/app/cocktail.service.ts
+++ cocktails/src/app/cocktail.service.ts
@@ -10,11 +10,16 @@ export class CocktailService {
 
   allCocktails() {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-    let obsC = this.http.get(url);
-    return obsC;
+    let obs = this.http.get(url);
+    return obs;
   }
   searchC(query:string) {
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
+    let obs = this.http.get(url);
+    return obs;
+  }
+  searchId(id:string) {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
     let obs = this.http.get(url);
     return obs;
   }
--- cocktails/src/app/cocktail/cocktail.component.ts
+++ cocktails/src/app/cocktail/cocktail.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CocktailService } from '../cocktail.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-cocktail',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./cocktail.component.css']
 })
 export class CocktailComponent {
+  cocktail:any;
+
+  constructor(
+    private service: CocktailService,
+    private route: ActivatedRoute,
+    ){}
+
+    ngOnInit(): void {
+      this.route.paramMap.subscribe(this.getRouterParam);
+    }
+
+    getRouterParam = (params:ParamMap) =>{
+      let cocktailId = params.get('id')
+      if(cocktailId){
+        this.service.searchId(cocktailId).subscribe((data) => {this.cocktail=data; console.log(data)})
+      }
+    }
 
 }
